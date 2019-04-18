ef62851de71b5b2bc735cd1afa2296b63a0cef18
tanialopezab <tania.lopez.ab@gmail.com>
2019-04-18 19:31:31 +0200

    create a planification

--- exercice8/main.js
+++ exercice8/main.js
@@ -1,2 +1,14 @@
 'use strict';
 console.log('hola');
+
+//Elementos html contendor cara, select boton
+
+//selecciono un estado del select y al pulsar el boton se pinta la cara feliz o la cara triste y se elige un numero aleatorio jasta 100 y si es par se pone un color amarillo de fondo y si no se pone el naranja
+
+//Recojo los elementos html(contenedor, select y boton)
+//poner listener al boton
+//crear la funcion handler; recogera el estado del select y lo pinta en pantalla
+//generara un nummerp aleatorio si es par amarillo y si es impar naranja
+
+
+//diseño lo Ultimo
